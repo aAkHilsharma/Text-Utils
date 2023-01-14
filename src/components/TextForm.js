04af66945224237fb1ba4366eaed9e14eec664f1
Akhil Sharma <akhilsharma6721@gmail.com>
2023-01-14 18:17:02 +0530

    Added Components

---
+++ src/components/TextForm.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function TextForm(props) {
+    return(
+        <div>
+            <h1 className="mb-3">{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary">Conver to uppercase</button>
+        </div>
+    )
+}
