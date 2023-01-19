4d7f56f5a2ab2133205d9ba0b6460e09468d5e30
Akhil Sharma <akhilsharma6721@gmail.com>
2023-01-19 19:11:53 +0530

    deployment changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { useState }  from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import About from './components/About';
-import { Routes, BrowserRouter, Route} from "react-router-dom"
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -31,18 +29,14 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
       <div className="App">
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
-            <Route exact path="/" element={<TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert}/>}/>
-            <Route exact path="/about" element={<About />}/>
-          </Routes>
+          <TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert}/>
+            {/* <Route exact path="/about" element={<About />}/> */}
         </div>
     </div>
-    </BrowserRouter>
   );
 }
 
